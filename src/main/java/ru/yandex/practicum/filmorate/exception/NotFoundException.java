e1ca00f711e66605f9758857ef4789dd0c00d16b
raijin <osmonco-ol@yandex.ru>
2024-08-12 18:57:17 +0300

    fix checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exception/NotFoundException.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.exception;
 public class NotFoundException extends RuntimeException {
     long id;
     String message;
+
     public NotFoundException(long id) {
         this.id = id;
     }
